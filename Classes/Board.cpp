47d0f3ac620b667de7d68edc5ad36ac3fe53524f
Valentin Mary <39530176+Vrock691@users.noreply.github.com>
2024-12-10 09:33:18 +0000

    feat: Création du board fonctionnelle

--- Classes/Board.cpp
+++ Classes/Board.cpp
@@ -7,21 +7,21 @@
 #include <ctime>
 #include <vector>
 
-Board::Board(){
-    _map_cases = {{"commun", {{Acier, 6},{Bois , 10}, {Sable , 9}, {Ble , 9}, {Nourriture, 8}}},{"rare" , {{Pierre , 4}}}, {"épique", {{Argent,2}, {Or , 1}}}};
-    _map_dice = {{"commun" , {5,6,7,8,9}}, {"rare", {3,4,10,11}}, {"épique" , {1,2,12}}};
+Board::Board()
+{
+    _map_cases = {{"commun", {{Acier, 6}, {Bois, 10}, {Sable, 9}, {Ble, 9}, {Nourriture, 8}}}, {"rare", {{Pierre, 4}}}, {"épique", {{Argent, 2}, {Or, 1}}}};
+    _map_dice = {{"commun", {5, 6, 7, 8, 9}}, {"rare", {3, 4, 10, 11}}, {"épique", {1, 2, 12}}};
 
-    int column = 0;
-    char row = 'a';
-
-    std::string index = row + std::to_string(column);
-    for (int i = 0; i < 7; i++)
+    // Création des cellules indépendantes
+    for (char row = 'a'; row < 'h'; row++)
     {
-        for (int j = 0; j < 7; j++)
+        for (int column = 0; column < 7; column++)
         {
+            std::string index = row + std::to_string(column);
+
             // Tirage de la ressource aléatoire
-            Ressource* cellRessource = new Ressource;
-            std::string* cellRarity = new std::string;
+            Ressource *cellRessource = new Ressource;
+            std::string *cellRarity = new std::string;
             drawRessource(cellRessource, cellRarity);
 
             // Choix du numéro du dés
@@ -32,20 +32,157 @@ Board::Board(){
 
             delete cellRessource;
             delete cellRarity;
-            column++;
         }
-        column = 0;
-        row++;
+    }
+
+    // Liaison des cellules entres elles
+    for (char row = 'a'; row < 'h'; row++)
+    {
+        for (int column = 0; column < 7; column++)
+        {
+            std::string index = row + std::to_string(column);
+            if (index == "a0")
+            {
+                std::string indexRight = row + std::to_string(column + 1);
+                Cell *rightCell = _board[indexRight];
+
+                std::string indexBottom = char(row + 1) + std::to_string(column);
+                Cell *bottomCell = _board[indexBottom];
+
+                _board[index]->setRightcell(rightCell);
+                _board[index]->setBottomcell(bottomCell);
+                break;
+            }
+
+            if (index == "a6")
+            {
+                std::string indexLeft = row + std::to_string(column - 1);
+                Cell *leftCell = _board[indexLeft];
+
+                std::string indexBottom = char(row + 1) + std::to_string(column);
+                Cell *bottomCell = _board[indexBottom];
+
+                _board[index]->setLeftcell(leftCell);
+                _board[index]->setBottomcell(bottomCell);
+                break;
+            }
+
+            if (index == "g0")
+            {
+                std::string indexTop = char(row - 1) + std::to_string(column);
+                Cell *topCell = _board[indexTop];
+
+                std::string indexRight = row + std::to_string(column + 1);
+                Cell *rightCell = _board[indexRight];
+
+                _board[index]->setTopcell(topCell);
+                _board[index]->setRightcell(rightCell);
+                break;
+            }
+
+            if (index == "g6")
+            {
+                std::string indexTop = char(row - 1) + std::to_string(column);
+                Cell *topCell = _board[indexTop];
+
+                std::string indexLeft = row + std::to_string(column - 1);
+                Cell *leftCell = _board[indexLeft];
+
+                _board[index]->setTopcell(topCell);
+                _board[index]->setLeftcell(leftCell);
+                break;
+            }
+
+            if (row == 'a')
+            {
+                std::string indexLeft = row + std::to_string(column - 1);
+                std::string indexRight = row + std::to_string(column + 1);
+                std::string indexBottom = char(row + 1) + std::to_string(column);
+
+                Cell *leftCell = _board[indexLeft];
+                Cell *rightCell = _board[indexRight];
+                Cell *bottomCell = _board[indexBottom];
+
+                _board[index]->setLeftcell(leftCell);
+                _board[index]->setRightcell(rightCell);
+                _board[index]->setBottomcell(bottomCell);
+                break;
+            }
+
+            if (row == 'g')
+            {
+                std::string indexLeft = row + std::to_string(column - 1);
+                std::string indexRight = row + std::to_string(column + 1);
+                std::string indexTop = char(row - 1) + std::to_string(column);
+
+                Cell *leftCell = _board[indexLeft];
+                Cell *rightCell = _board[indexRight];
+                Cell *topCell = _board[indexTop];
+
+                _board[index]->setLeftcell(leftCell);
+                _board[index]->setRightcell(rightCell);
+                _board[index]->setTopcell(topCell);
+                break;
+            }
+
+            if (column == 0)
+            {
+                std::string indexRight = row + std::to_string(column + 1);
+                std::string indexTop = char(row - 1) + std::to_string(column);
+                std::string indexBottom = char(row + 1) + std::to_string(column);
+
+                Cell *rightCell = _board[indexRight];
+                Cell *topCell = _board[indexTop];
+                Cell *bottomCell = _board[indexBottom];
+
+                _board[index]->setRightcell(rightCell);
+                _board[index]->setTopcell(topCell);
+                _board[index]->setBottomcell(bottomCell);
+                break;
+            }
+
+            if (column == 6)
+            {
+                std::string indexTop = char(row - 1) + std::to_string(column);
+                std::string indexBottom = char(row + 1) + std::to_string(column);
+                std::string indexLeft = row + std::to_string(column - 1);
+
+                Cell *topCell = _board[indexTop];
+                Cell *bottomCell = _board[indexBottom];
+                Cell *leftCell = _board[indexLeft];
+
+                _board[index]->setTopcell(topCell);
+                _board[index]->setBottomcell(bottomCell);
+                _board[index]->setLeftcell(leftCell);
+                break;
+            }
+
+            std::string indexTop = char(row - 1) + std::to_string(column);
+            std::string indexBottom = char(row + 1) + std::to_string(column);
+            std::string indexLeft = row + std::to_string(column - 1);
+            std::string indexRight = row + std::to_string(column + 1);
+
+            Cell *topCell = _board[indexTop];
+            Cell *bottomCell = _board[indexBottom];
+            Cell *leftCell = _board[indexLeft];
+            Cell *rightCell = _board[indexRight];
+
+            _board[index]->setTopcell(topCell);
+            _board[index]->setBottomcell(bottomCell);
+            _board[index]->setLeftcell(leftCell);
+            _board[index]->setRightcell(rightCell);
+        }
     }
 };
 
-int randomInt() 
+int randomInt()
 {
     std::srand(std::time(0));
     return std::rand();
 }
 
-void Board::drawRessource(Ressource* _ressourceRef, std::string* _rarityRef) {
+void Board::drawRessource(Ressource *_ressourceRef, std::string *_rarityRef)
+{
     *_rarityRef = "";
     *_ressourceRef = undefined;
 
@@ -55,14 +192,17 @@ void Board::drawRessource(Ressource* _ressourceRef, std::string* _rarityRef) {
     {
         // Choix de la rareté
         std::string selectedRarity = listRarity[randomInt() % 3];
-        
+
         // Vérification qu'il existe encore des ressources disponibles
-        if (!_map_cases[selectedRarity].empty()) {
+        if (!_map_cases[selectedRarity].empty())
+        {
 
             // Récupération des ressources disponibles
             std::vector<Ressource> availableRessources = {};
-            for (const auto& [key, value] : _map_cases[selectedRarity]) {
-                if (value!= 0){
+            for (const auto &[key, value] : _map_cases[selectedRarity])
+            {
+                if (value != 0)
+                {
                     availableRessources.push_back(key);
                 }
             }
